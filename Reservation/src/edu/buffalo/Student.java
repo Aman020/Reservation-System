783a705ed94cbf9243c0d412b692a83ff8d19f71
a_m_a_n <amanbhayana@yahoo.in>
2019-05-10 21:50:59 -0400

    Added comments

--- Reservation/src/edu/buffalo/Student.java
+++ Reservation/src/edu/buffalo/Student.java
@@ -1,11 +1,18 @@
 package edu.buffalo;
 import java.util.*;
 
+
+/**
+ *The Student class is a worker class that encapsulates student related information.
+ * @author 	Meghana,Prabhleen,Aman,Amlan,Deboshree
+ * @version	1.0
+ * @since	2019-05-03
+ */
+
 public class Student {
 
     private String emailId;
     private Date banStartDate;
-    
     
     public Student(String emailId, Date banStartDate )
     {
@@ -14,21 +21,22 @@ public class Student {
     }
 
 
+    //Get the email id of the student
 	public String getEmailId() {
 		return emailId;
 	}
 
-
+	//Set the email id of a student
 	public void setEmailId(String emailId) {
 		this.emailId = emailId;
 	}
 
-
+	//Get the banned date of the student
 	public Date getBanStartDate() {
 		return banStartDate;
 	}
 
-
+	//Set the banned date of the student
 	public void setBanStartDate(Date banStartDate) {
 		this.banStartDate = banStartDate;
 	}
